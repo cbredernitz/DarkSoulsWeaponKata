86ed88fb518d877dd989ef7f2f1e3f3f2f92d91c
Chris <chrisrbredernitz@gmail.com>
2022-05-17 17:16:52 -0600

    add builder to model. Add csv parser annotations to try out

--- src/main/java/com/example/darksoulsproject/weapons/models/WeaponsModel.java
+++ src/main/java/com/example/darksoulsproject/weapons/models/WeaponsModel.java
@@ -1,24 +1,60 @@
 package com.example.darksoulsproject.weapons.models;
 
+import com.opencsv.bean.CsvBindByName;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class WeaponsModel {
+    @CsvBindByName(column = "Name")
     String name;
+
+    @CsvBindByName(column = "Category")
     Category category;
+
+    @CsvBindByName(column = "Reinforcement")
     Reinforcement reinforcement;
+
+    @CsvBindByName(column = "Damage")
     String damage;
+
+    @CsvBindByName(column = "Damage Reduction")
     String damageReduction;
+
+    @CsvBindByName(column = "Aux Effects")
     String auxEffects;
+
+    @CsvBindByName(column = "Stat Requirements")
     String statRequirements;
+
+    @CsvBindByName(column = "Stat Bonuses")
     String statBonuses;
+
+    @CsvBindByName(column = "Critical")
     Integer critical;
+
+    @CsvBindByName(column = "Weight")
     Float weight;
+
+    @CsvBindByName(column = "Stability")
     Integer stability;
-    Integer Durability;
+
+    @CsvBindByName(column = "Durability")
+    Integer durability;
+
+    @CsvBindByName(column = "Sell Price")
     Integer sellPrice;
+
+    @CsvBindByName(column = "Spell Buff")
     String spellBuff;
+
+    @CsvBindByName(column = "Range")
     String range;
+
+    @CsvBindByName(column = "Buffable")
     boolean buffable;
+
+    @CsvBindByName(column = "Infusable")
     boolean infusable;
 }
